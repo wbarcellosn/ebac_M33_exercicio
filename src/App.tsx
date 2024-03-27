aa6d1098d453e36e3b9a07294e4218f2ea236023
wbarcellosn <wbarcellosn@gmail.com>
2024-03-27 20:15:38 -0300

    Primeiro commit: Adição dos testes do componente PostComments

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://http2.mlstatic.com/D_NQ_NP_657801-MLB20406239250_092015-O.webp">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
+import App from '../../App';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve inserir dois comentários', () => {
+        const {debug} = render(<App />)
+
+        const comentarios = ['Comentário 1', 'Comentário 2']
+
+        comentarios.forEach((comentario) => {
+            addComentario(comentario)
+            // eslint-disable-next-line testing-library/no-debugging-utils
+            debug()
+            expect(screen.getByText(comentario)).toBeInTheDocument()
+        })
+
+        function addComentario(comentario: string) {
+            fireEvent.change(screen.getByTestId('textarea-comentario'), {
+                target: { value: comentario }
+            });
+    
+            fireEvent.click(screen.getByTestId('btn-comentar'));
+        }        
+    })
 });
